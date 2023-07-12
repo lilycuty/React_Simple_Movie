e17efcc904a19f82ddddff7754f29e2df9e402e6
THAI <xinphatmutikill@gmail.com>
2023-07-13 03:19:16 +0700

    Update

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -3,29 +3,52 @@ import { fetcher } from '../config';
 import MovieCart from '../components/movie/MovieCart';
 import { useEffect, useState } from 'react';
 import useDebounce from '../hooks/useDebounce';
+import ReactPaginate from 'react-paginate';
+
+// const pageCount = 5;
+const itemsPerPage = 20;
 
 const MoviePage = () => {
+	const [itemOffset, setItemOffset] = useState(0);
+	const [pageCount, setPageCount] = useState(0);
+
+	const [nextPage, setNextPage] = useState(1);
 	const [filter, setFilter] = useState('');
+
 	const [url, setUrl] = useState(
-		`https://api.themoviedb.org/3/movie/popular?api_key=a47ce0306fa5c2cb2ddf7f640a085e1f`
+		`https://api.themoviedb.org/3/movie/popular?api_key=a47ce0306fa5c2cb2ddf7f640a085e1f&page=${nextPage}`
 	);
 	const filterDebounce = useDebounce(filter, 500);
 	const handleFilterChange = (e) => {
 		setFilter(e.target.value);
 	};
-	const { data } = useSWR(url, fetcher);
+	const { data, error } = useSWR(url, fetcher);
 	useEffect(() => {
 		if (filterDebounce) {
 			setUrl(
-				`https://api.themoviedb.org/3/search/movie?api_key=a47ce0306fa5c2cb2ddf7f640a085e1f&query=${filterDebounce}`
+				`https://api.themoviedb.org/3/search/movie?api_key=a47ce0306fa5c2cb2ddf7f640a085e1f&query=${filterDebounce}&page=${nextPage}`
 			);
 		} else {
 			setUrl(
-				`https://api.themoviedb.org/3/movie/popular?api_key=a47ce0306fa5c2cb2ddf7f640a085e1f`
+				`https://api.themoviedb.org/3/movie/popular?api_key=a47ce0306fa5c2cb2ddf7f640a085e1f&page=${nextPage}`
 			);
 		}
-	}, [filterDebounce]);
+	}, [filterDebounce, nextPage]);
+
+	const loading = !data && !error;
 	const movies = data?.results || [];
+	// if (!data) return null;
+	// const { page, total_pages } = data;
+	// console.log(page, total_pages);
+	useEffect(() => {
+		if (!data || !data.total_results) return;
+		setPageCount(Math.ceil(data.total_results / itemsPerPage));
+	}, [data, itemOffset]);
+	const handlePageClick = (event) => {
+		const newOffset = (event.selected * itemsPerPage) % data.total_results;
+		setItemOffset(newOffset);
+		setNextPage(event.selected + 1);
+	};
 	return (
 		<div className="py-10 page-container">
 			<div className="flex mb-10">
@@ -54,12 +77,79 @@ const MoviePage = () => {
 					</svg>
 				</button>
 			</div>
+			{loading && (
+				<div className="w-10 h-10 rounded-full border-4 border-primary border-t-transparent border-t-4 mx-auto animate-spin"></div>
+			)}
 			<div className="grid grid-cols-4 gap-10">
-				{movies?.length > 0 &&
+				{!loading &&
+					movies?.length > 0 &&
 					movies.map((item) => (
 						<MovieCart key={item.id} item={item}></MovieCart>
 					))}
 			</div>
+			<div className="mt-10">
+				<ReactPaginate
+					breakLabel="..."
+					nextLabel="next >"
+					onPageChange={handlePageClick}
+					pageRangeDisplayed={5}
+					pageCount={pageCount}
+					previousLabel="< previous"
+					renderOnZeroPageCount={null}
+					className="pagination"
+				/>
+			</div>
+			{/* <div className="flex items-center justify-center mt-10 gap-x-5">
+				<span
+					className="cursor-pointer"
+					onClick={() => setNextPage(nextPage - 1)}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth="1.5"
+						stroke="currentColor"
+						className="w-6 h-6"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M15.75 19.5L8.25 12l7.5-7.5"
+						/>
+					</svg>
+				</span>
+
+				{new Array(pageCount).fill(0).map((item, index) => (
+					<span
+						className="cursor-pointer inline-block px-4 py-2 bg-white text-slate-900 leading-none rounded-lg"
+						key={item.id}
+						onClick={() => setNextPage(index + 1)}
+					>
+						{index + 1}
+					</span>
+				))}
+
+				<span
+					className="cursor-pointer"
+					onClick={() => setNextPage(nextPage + 1)}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth="1.5"
+						stroke="currentColor"
+						className="w-6 h-6"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M8.25 4.5l7.5 7.5-7.5 7.5"
+						/>
+					</svg>
+				</span>
+			</div> */}
 		</div>
 	);
 };
